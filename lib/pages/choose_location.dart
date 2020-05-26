cbd8780394bca30ce74548cb0315d9a3ab4907d4
Sourabh Shegane <sgsourabh30@gmail.com>
2020-05-26 16:31:04 +0530

    Added ListView with CardView and dynamic data

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,21 +1,59 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterapp/services/world_time.dart';
 class ChooseLocation extends StatefulWidget {
   @override
   _ChooseLocationState createState() => _ChooseLocationState();
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
+  List<WorldTime> worldTimes = [
+    WorldTime(url: "asia/kolkata", location: "Mumbai"),
+    WorldTime(url: 'Europe/Berlin', location: 'Athens'),
+    WorldTime(url: 'Africa/Cairo', location: 'Cairo'),
+    WorldTime(url: 'Africa/Nairobi', location: 'Nairobi'),
+    WorldTime(url: 'America/Chicago', location: 'Chicago'),
+    WorldTime(url: 'America/New_York', location: 'New York'),
+    WorldTime(url: 'Asia/Seoul', location: 'Seoul'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta'),
+  ];
+
+  void updateTime(index) async{
+    WorldTime instance =  worldTimes[index];
+    await instance.getTime();
+
+    //Navigate to home screen and pass data
+      Navigator.pop(context, {
+        'location': instance.location, 'time': instance.time, 'isDayTime' : instance.isDayTime
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
       appBar: AppBar(
         backgroundColor: Colors.blue[900],
         title: Text("Choose Location"),
         centerTitle: true,
         elevation: 0,
       ),
-      body: Text("Choose Location"),
+      body: ListView.builder(
+        itemCount: worldTimes.length,
+        itemBuilder: (context, index){
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 4),
+            child: Card(
+              child: ListTile(
+                onTap: (){
+                  updateTime(index);
+                },
+                title: Text(worldTimes[index].location),
+              ),
+            ),
+          );
+        },
+      )
     );
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -10,12 +12,15 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    data = ModalRoute.of(context).settings.arguments;
+    data = data.isNotEmpty ? data : ModalRoute.of(context).settings.arguments;
 
     //Set Background
 
     String bgImage = data['isDayTime'] ? 'day.png' : 'night.png';
+    Color bgColor = data['isDayTime'] ? Colors.blue : Colors.indigo[700];
+
     return Scaffold(
+      backgroundColor: bgColor,
       body: SafeArea(
         child: Container(
           decoration: BoxDecoration(
@@ -29,18 +34,37 @@ class _HomeState extends State<Home> {
             child: Column(
               children: [
                 FlatButton.icon(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/location');
+                    onPressed: () async{
+                      dynamic result = await Navigator.pushNamed(context, '/location');
+                      setState(() {
+                        data = {
+                          'time' : result['time'],
+                          'location' : result['location'],
+                          'isDayTime' : result['isDayTime']
+                        };
+                      });
                     },
-                    icon: Icon(Icons.edit_location),
-                    label: Text("Edit Location")),
+                    icon: Icon(
+                        Icons.edit_location,
+                        color: Colors.grey[300],
+                    ),
+                    label: Text(
+                        "Edit Location",
+                      style: TextStyle(
+                        color: Colors.grey[300]
+                      ),
+                    )),
                 SizedBox(height: 20),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Text(
                       data['location'],
-                      style: TextStyle(fontSize: 28, letterSpacing: 2),
+                      style: TextStyle(
+                          fontSize: 28,
+                          letterSpacing: 2,
+                          color : Colors.white
+                      ),
                     ),
                   ],
                 ),
